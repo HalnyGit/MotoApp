0017d06ff5dc37439e92a67ee71082ce1696b54b
HalnyGit <haliniakm@gmail.com>
2023-05-21 16:19:02 +0200

    xml

--- MotoApp/MotoApp/App.cs
+++ MotoApp/MotoApp/App.cs
@@ -1,7 +1,8 @@
 ﻿namespace MotoApp;
 
 using MotoApp.Components.CsvReader;
-
+using System;
+using System.Xml.Linq;
 
 public class App : IApp
 {
@@ -13,10 +14,39 @@ public class App : IApp
 
     public void Run()
     {
-        var cars = _csvReader.ProcessCars("Resources\\Files\\fuel.csv");
+        CreateXml();
+        QueryXml();
+
     }
 
+    private void QueryXml()
+    {
+        var document = XDocument.Load("fuel.xml");
+        var names = document
+            .Element("Cars")?
+            .Elements("Car")
+            .Where(x => x.Attribute("Manufacturer")?.Value == "BMW")
+            .Select(x => x.Attribute("Name")?.Value);
 
+        foreach (var name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
 
+    private void CreateXml()
+    {
+        var records = _csvReader.ProcessCars("Resources\\Files\\fuel.csv");
 
+        var document = new XDocument();
+        var cars = new XElement("Cars", records
+            .Select(x =>
+                new XElement("Car",
+                    new XAttribute("Name", x.Name),
+                    new XAttribute("Year", x.Year),
+                    new XAttribute("Manufacturer", x.Manufacturer)
+            )));
+        document.Add(cars);
+        document.Save("fuel.xml");
+    }
 }
